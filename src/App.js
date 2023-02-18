beaf3802914457427e357792848b31f5271b2b91
Lowza <jackrlomax@gmail.com>
2023-02-18 18:26:53 +0000

    add component sections for projects and contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import UnderscoreSpring from './modules/UnderscoreSpring';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -17,9 +19,11 @@ function App() {
         <section>
           <h2>About me</h2>
           <p>My name is Jack Lomax, I am a full stack web developer based in Axminster, Devon, UK. I started learning web development from home in 2023 through online courses and resources.</p>
-          <p>I am passionate about building intuitive and eloquent websites and am looking for a junior dev position to finally kick start my career and learn among professionals.</p>
+          <p>I am passionate about building intuitive and eloquent websites and am looking for a junior dev position to kick start my career and learn among professionals.</p>
         </section>
         <Skills />
+        <Projects />
+        <Contact />
       </main>
     </div>
   );
