d00cfa2e0db028e734519c0c8d858914d3733a83
Lowza <jackrlomax@gmail.com>
2023-02-19 01:27:27 +0000

    add flexbox for skills component list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,12 +10,12 @@ function Skills () {
     <section id="skills">
         <h2>Skills</h2>
         <ol>
-            <li>Javascript ES6(+)</li><img src={jsLogo} alt="JavaScript icon" />
-            <li>React</li><img src={reactLogo} alt="React icon" />
-            <li>Node.js</li><img src={nodeLogo} alt="Node icon" />
-            <li>Express</li><img id="expressLogo" src={expressLogo} alt="Express icon" />
-            <li>SQL</li><img src={sqlLogo} alt="SQL icon" />
-            <li>Git</li><img src={gitLogo} alt="Git icon" />
+            <li>Javascript ES6(+)<img src={jsLogo} alt="JavaScript icon" /></li>
+            <li>React<img src={reactLogo} alt="React icon" /></li>
+            <li>Node.js<img src={nodeLogo} alt="Node icon" /></li>
+            <li>Express<img id="expressLogo" src={expressLogo} alt="Express icon" /></li>
+            <li>SQL<img src={sqlLogo} alt="SQL icon" /></li>
+            <li>Git<img src={gitLogo} alt="Git icon" /></li>
         </ol>
     </section>
     )
