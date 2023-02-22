5ad2ac56097961532363f71cf69bf22c1b94be43
Lowza <jackrlomax@gmail.com>
2023-02-22 15:07:09 +0000

    add basic form with submit functionality via Formsubmit.co, add contact.css stylesheet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,21 @@
+import "./contact.css";
+
 function Contact() {
     return (
-        <section id="contact">
+        <section id="contact">      
             <h2>Contact me</h2>
+            <form action="https://formsubmit.co/jackrlomax@gmail.com" method="POST">
+                <label>Name</label>
+                <input placeholder="Name" type="text" name="name"/>
+
+                <label>Email</label>
+                <input placeholder="Email" type="email" name="email"/>
+
+                <label>Message</label>
+                <textarea placeholder="Message" type="textarea" name="message"/>
+
+                <button type="submit">Submit</button>
+            </form>
         </section>
     )
 }
