d9080563e37df2348e48516abe1e38106882fcea
Jack <jackrlomax@gmail.com>
2024-02-27 15:45:58 +0000

    fix responsiveness of background gradients

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className} bg-gray-50 dark:bg-gray-900 text-gray-950 dark:text-gray-50 dark:text-opacity-90 relative pt-28 sm:pt-36`}>
 
-        <div className='bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263] xl:right-[40rem] 2xl:right-[45rem]'></div>
+      <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
 
-        <div className='bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[33rem] lg:left-[-30rem] xl:left-[-35rem] 2xl:left-[45rem] dark:bg-[#676394]'></div>
+      <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
